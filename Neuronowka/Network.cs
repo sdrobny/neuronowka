cbb997183e390d48fa4621f5b2fb1ad7e061f25a
sdrobny <szymondrobny@gmail.com>
2019-01-29 12:01:15 +0100

    skalowanie wartosci wejsciowych do (0,1)

--- Neuronowka/Network.cs
+++ Neuronowka/Network.cs
@@ -86,5 +86,54 @@ namespace Neuronowka
             }
             return NewInputs;
         }
+
+        //Funkcja liczaca max wartosc kolumny
+        private double Max(int col, List<List<Double>> dataset)
+        {
+            double max = double.MinValue;
+
+            for(int i = 0; i < dataset.Count; i++)
+            {
+                if (dataset[i][col] > max) max = dataset[i][col];
+            }
+
+            return max;
+        }
+
+        //Funkcja liczaca minimalna wartosc kolumny
+        private double Min(int col, List<List<Double>> dataset)
+        {
+            double min = double.MaxValue;
+
+            for (int i = 0; i < dataset.Count; i++)
+            {
+                if (dataset[i][col] < min) min = dataset[i][col];
+            }
+
+            return min;
+        }
+
+        //Przeskalowanie wartosci na zakres 0 - 1 (Funkcji sigmoidalnej)
+        public List<List<Double>> NormalizeData(List<List<Double>> dataset)
+        {
+            //Wartosci min kolumn:
+            List<Double> MinValues = new List<Double>();
+            for (int i = 0; i < dataset[0].Count; i++) MinValues.Add(Min(i, dataset));
+
+            //Wartosci min kolumn:
+            List<Double> MaxValues = new List<Double>();
+            for (int i = 0; i < dataset[0].Count; i++) MaxValues.Add(Max(i, dataset));
+
+
+            foreach (List<Double> row in dataset)
+            {
+                for(int i =  0; i < row.Count; i++)
+                {
+                    row[i] = (row[i] - MinValues[i]) / (MaxValues[i] - MinValues[i]);
+                }
+            }
+
+            return dataset;
+        }
     }
 }
